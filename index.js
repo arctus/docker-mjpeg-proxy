3c2d54b01fd9a3e68a89acbb6ee76e1976efba9d
Pavel Cuchriajev <pavelsm@gmail.com>
2020-05-12 09:41:18 +0300

    FIX: Minor bug.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var mjpegurl = process.env.MJPEG_URL;
 
 var urls = mjpegurl.split(",");
-for (var i = 0; i < arr.length; i++) {
+for (var i = 0; i < urls.length; i++) {
     app.get(util.format('/index_%d.mjpg', i + 1), new MjpegProxy(urls[i]).proxyRequest);
 }
 app.listen(process.env.MJPEG_PORT);
