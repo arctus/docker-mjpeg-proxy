8d06455b6068379208aeb0cc8af9ed08b76920a3
Pavel Cuchriajev <pavelsm@gmail.com>
2020-05-12 09:08:27 +0300

    ADD: Capability to specify more than one stream.

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ var express = require('express');
 var app = express();
 var mjpegurl = process.env.MJPEG_URL;
 
-app.get('/index.jpg', new MjpegProxy(mjpegurl).proxyRequest);
+var urls = mjpegurl.split(",");
+for (var i = 0; i < arr.length; i++) {
+    app.get(util.format('/index_%d.mjpg', i + 1), new MjpegProxy(urls[i]).proxyRequest);
+}
 app.listen(process.env.MJPEG_PORT);
